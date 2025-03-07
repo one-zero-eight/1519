fc7ded5227ef693e227a4a5c967cf7b3ffe93b00
Projacktor <1234xl.com@gmail.com>
2025-03-07 23:26:32 +0300

    chore: update sidebar and layout

--- frontend/src/components/Checkbox-filter.tsx
+++ frontend/src/components/Checkbox-filter.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { FormGroup, Checkbox, FormControlLabel, SvgIcon } from '@mui/material'
 
 interface CheckboxFilterProps {
-  options: { icon: React.ReactNode; name: string }[]
+  options: { icon: React.ReactNode; name: string; color: string }[]
   onChange: (selected: string[]) => void
 }
 
@@ -25,18 +25,30 @@ function CheckboxFilter({ options, onChange }: CheckboxFilterProps) {
         sx={{
           display: 'flex',
           flexDirection: 'row',
-          gap: '8px',
-          width: '100%'
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          width: '100%',
+          ml: '1rem'
         }}
       >
         {options.map((option) => (
           <FormControlLabel
+            className={`flex items-center justify-center rounded-xl pl-2 pr-2`}
+            sx={{
+              bgcolor: option.color
+            }}
             key={option.name}
             control={
               <Checkbox
                 checked={selected.includes(option.name)}
                 onChange={() => handleChange(option.name)}
                 name={option.name}
+                sx={{
+                  '&.MuiSvgIcon-root': { fontSize: 28 },
+                  '&.Mui-checked': {
+                    color: 'white'
+                  }
+                }}
               />
             }
             label={<SvgIcon>{option.icon}</SvgIcon>}
