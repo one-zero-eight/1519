925a88c6ae4ce7fb5e3989109959e41db3730000
Projacktor <1234xl.com@gmail.com>
2025-06-13 17:11:32 +0300

    chore: add dotenv and start api

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
+import * as dotenv from 'dotenv'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SERVER: process.env.NEXT_PUBLIC_SERVER
+  }
 }
 
 export default nextConfig
---
+++ frontend/src/lib/api/patron.ts
@@ -0,0 +1,14 @@
+import { PatronApplication } from '@/types/types'
+
+const apiServer = process.env.NEXT_PUBLIC_SERVER
+
+export async function getApplications(): Promise<PatronApplication> {
+  const res = await fetch(`${apiServer}/patron/me/rated-applications/`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  if (!res.ok) throw new Error('Create message: network response was not ok')
+  return res.json()
+}
