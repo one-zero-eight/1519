118db7bc9190b1642952acf093db76ef9fe737db
Projacktor <1234xl.com@gmail.com>
2025-06-26 23:07:14 +0300

    BREAKING CHANGE: change configuration to tanstack/router-vite-react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,20 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [
+    tanstackRouter({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
